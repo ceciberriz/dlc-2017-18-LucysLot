6c3595f8d72fb95527ec02c0e0d776a823f4b78d
ceci <cecilia_berriz@brown.edu>
2018-01-21 15:59:14 -0500

    astarscene

---
+++ Assets/Scripts/DialogueButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueButton : MonoBehaviour {
+
+    private TextManager textManager;
+
+    // Use this for initialization
+    void Start()
+    {
+        textManager = gameObject.AddComponent(typeof(TextManager)) as TextManager;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnClick()
+    {
+        Debug.Log("mouse down!!!");
+        //textManager.SetButtonClicked();
+    }
+}
+
