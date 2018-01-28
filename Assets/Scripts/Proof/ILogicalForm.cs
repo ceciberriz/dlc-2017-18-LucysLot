38ae8a4c0b472be6d808aa8d50aff2b1f6207fbe
Bill Marcy <william_marcy@brown.edu>
2018-01-28 15:28:55 -0500

    trying to merge stuff

--- Assets/Scripts/Proof/ILogicalForm.cs
+++ Assets/Scripts/Proof/ILogicalForm.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public interface ILogicalForm {
-	ISemanticType getType();
+	ISemanticType GetSemanticType();
 	ISemanticValue Denotation(Model m);
 
 	bool IsFormula();
